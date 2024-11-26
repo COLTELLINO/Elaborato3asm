f2f781efb425d6dd02ec35efd077da0c30b59d10
Filippo Patrignani <filippo.patrignani2@studio.unibo.it>
2024-11-26 12:05:38 +0100

    elaborato 3 finito

---
+++ Source.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+void main()
+{
+	// Input
+	unsigned char vet[] = { 2,4,67,2,2,58,99 };
+	unsigned int len = 55;	// lunghezza (numero di bit)
+	unsigned char n = 4;	// numero di bit dati
+
+	// Ouput
+	unsigned char errori = 0;	// 1 = errori; 0 = no errori
+
+	__asm
+	{
+		xor esi, esi // azzero esi
+		xor ecx, ecx // azzero ecx
+		xor ebx, ebx // azzero ebx
+		xor edi, edi // azzero edi
+		xor edx, edx // azzero edx
+		inc n
+
+	cicloscorrimento :
+		mov al, [vet + edi] // metto in al il numero
+
+	ciclocontrollo :
+		inc esi		// counter globale bit
+		cmp esi, len        // controllo se ho finito
+		ja termina
+		test al, 1			// guardo se il bit meno significativo è a 1
+		jz controlloparita	// se il bit è a 0 vado avanti
+		inc bl				// se è a 1 incremento il contatore
+
+	controlloparita :
+		inc edx			//incremento il contatore dei bit fatti (8)
+		inc cl			//incremento il contatore dei bit fatti (n)
+		cmp cl, n		//guardo se sono arrivato fino a quello di parità
+		jne prossimobit // in caso contrario vado avanti
+		xor cl, cl		//resetto il counter di bit 
+		test bl, 1
+		jnz errore
+
+	prossimobit :
+		cmp edx, 8			//guardo se ho finito i bit della parola
+		jae prossimaparola	// in caso vado alla prossima parola
+		shr al, 1			//vado al prossimo bit
+		jmp ciclocontrollo
+
+	prossimaparola :
+		xor edx, edx			//resetto il counter dei bit 
+		inc edi					// vado all'elemento dopo di vet
+		jmp cicloscorrimento	// se non ci sono errori finisco
+
+	errore :
+		mov errori, 1 //sennò imposto l'errore
+
+	termina :
+		dec n
+	}
+
+	// Stampa su video
+	printf("La sequenza di bit %scontiene errori\n", (errori ? "" : "non "));
+}
